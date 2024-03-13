60a8fcd40186fced317244c99ed58db3111b7c41
Sai Mamidi <89061658+saimamidi01@users.noreply.github.com>
2024-03-13 00:07:05 -0700

    js

--- main.py
+++ main.py
@@ -11,15 +11,6 @@ app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 @app.get("/")
 def root():
    return FileResponse("./static/index.html")
-@app.get("/login.html")
-def open():
-   return FileResponse("./static/login.html")
-@app.get("/register.html")
-def open():
-   return FileResponse("./static/registration.html")
-@app.get("/adminlogin.html")
-def open():
-   return FileResponse("./static/adminlogin.html")
 @app.post("/register")
 #Form stuff is from https://fastapi.tiangolo.com/tutorial/request-forms/
 def register(username: Annotated[str, Form()], password: Annotated[str, Form()]):
