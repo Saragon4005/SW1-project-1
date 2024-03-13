65f96a66be96bb56680603cd71d595d22b630a24
Sai Mamidi <89061658+saimamidi01@users.noreply.github.com>
2024-03-12 22:44:02 -0700

    Added pythoncode
    Redirect to the different files. Gets the form infomation.

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from typing import Annotated
 
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
-
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+   return FileResponse("./static/index.html")
+@app.get("/login.html")
+def open():
+   return FileResponse("./static/login.html")
+@app.get("/register.html")
+def open():
+   return FileResponse("./static/registration.html")
+@app.get("/adminlogin.html")
+def open():
+   return FileResponse("./static/adminlogin.html")
+@app.post("/register")
+#Form stuff is from https://fastapi.tiangolo.com/tutorial/request-forms/
+def register(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+   #you can add the database stuff here 
+   return {"username": username, "password": password}
+@app.post("/loginPOST")
+def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+   # you can add the database stuff here.
+   return {"username": username, "password": password }
+   
 
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
