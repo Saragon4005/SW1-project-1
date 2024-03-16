723d5781c301197deecfe1d7a9cdb8dfedee7b96
Saragon4005 <raven.keszei@gmail.com>
2024-03-15 17:51:27 -0700

    format sql file

--- create.sql
+++ create.sql
@@ -1,15 +1,16 @@
-CREATE TABLE users (
-  username TEXT PRIMARY KEY,
-  password TEXT NOT NULL,
-  account_numbers TEXT  -- Store account numbers as a comma-separated string
-);
-CREATE TABLE admins (
-  username TEXT PRIMARY KEY,
-  password TEXT NOT NULL
-);
-CREATE TABLE accounts (
-  account_number INTEGER PRIMARY KEY,
-  pin INTEGER NOT NULL,
-  balance REAL NOT NULL DEFAULT 0  -- Default balance starts at 0
-);
+CREATE TABLE
+  users (
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL,
+    account_numbers TEXT -- Store account numbers as a comma-separated string
+  );
 
+CREATE TABLE
+  admins (username TEXT PRIMARY KEY, password TEXT NOT NULL);
+
+CREATE TABLE
+  accounts (
+    account_number INTEGER PRIMARY KEY,
+    pin INTEGER NOT NULL,
+    balance REAL NOT NULL DEFAULT 0 -- Default balance starts at 0
+  );
