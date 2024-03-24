5d8cfef4956081c69b19c608fd858eeb2ecb28cb
mapilado128 <rolypoly128@gmail.com>
2024-03-23 23:19:21 -0700

    RE:RE: fixed menu sidebar blur and dim effects and dimensions

--- static/script.js
+++ static/script.js
@@ -32,5 +32,6 @@ function passwordvalidate(){
 
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.sidebar').classList.toggle('active');
-    document.querySelector('.content').classList.toggle('active');
+    // document.querySelector('.content').classList.toggle('active');
+     document.body.classList.toggle('blur');
 });
