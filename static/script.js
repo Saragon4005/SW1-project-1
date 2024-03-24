2ff6a949f3669538c41aabedd18ce21467f5aa61
mapilado128 <rolypoly128@gmail.com>
2024-03-24 00:12:01 -0700

    added transaction buttons and js so user doesn't always have to directly click on hamburger btn to exit menu

--- static/script.js
+++ static/script.js
@@ -9,8 +9,21 @@ function reload(value){
         location.assign("/static/adminlogin.html")
     }
 }
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.sidebar').classList.toggle('active');
-    // document.querySelector('.content').classList.toggle('active');
-     document.body.classList.toggle('blur');
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.querySelector('.sidebar');
+    const menuBtn = document.querySelector('.menu-btn');
+    
+    menuBtn.addEventListener('click', function() {
+        sidebar.classList.toggle('active');
+        document.body.classList.toggle('blur');
+    });
+    document.addEventListener('click', function(event) {
+        const isSidebarClicked = sidebar.contains(event.target);
+        const isMenuBtnClicked = menuBtn.contains(event.target);
+
+        if (!isSidebarClicked && !isMenuBtnClicked) {
+            sidebar.classList.remove('active');
+            document.body.classList.remove('blur');
+        }
+    });
 });
