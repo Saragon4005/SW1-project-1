ca7ea2d5c74b776b68f0be3b3a78e62c7f591bdc
mapilado128 <rolypoly128@gmail.com>
2024-04-09 19:19:24 -0700

    added missing code from files: accountsettings.html, atmLogin.html, change-password.html, change-pin.html, change-username.html, index.css, member.css, member.html, menuPartial.html, script.js

--- static/script.js
+++ static/script.js
@@ -9,7 +9,56 @@ function reload(value){
         location.assign("/static/adminlogin.html");
     }
 }
-document.querySelector('.menu-btn').addEventListener('click', () => {
+document.querySslector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.sidebar').classList.toggle('active');
     document.querySelector('.content').classList.toggle('active');
+});function reload(value){
+    if(value === "login"){
+        location.assign("/static/login.html");
+    }
+    else if(value === "register"){
+        location.assign("/static/registration.html");
+    }
+    else if(value === "adminlogin"){
+        location.assign("/static/adminlogin.html");
+    }
+}
+
+function passwordvalidate(){
+    //Learned how to change form action from https://stackoverflow.com/a/5361776
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("cpassword").value;
+    let form = document.getElementById("formR");
+    if(password.length < 10){
+        let numberOfmissing = 10-password.length;
+        alert("password too short by " + numberOfmissing + " characters");
+        form.action="/passwordError";
+    }
+    
+    else if(password != confirmPassword){
+        alert("passwords do not match");
+        form.action="/passwordError";
+    }
+    else{
+       form.action= "/register";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.querySelector('.sidebar');
+    const menuBtn = document.querySelector('.menu-btn');
+
+    menuBtn.addEventListener('click', function() {
+        sidebar.classList.toggle('active');
+        document.body.classList.toggle('blur');
+    });
+    document.addEventListener('click', function(event) {
+        const isSidebarClicked = sidebar.contains(event.target);
+        const isMenuBtnClicked = menuBtn.contains(event.target);
+
+        if (!isSidebarClicked && !isMenuBtnClicked) {
+            sidebar.classList.remove('active');
+            document.body.classList.remove('blur');
+        }
+    });
 });
