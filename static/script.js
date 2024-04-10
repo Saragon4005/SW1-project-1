5b7da209a8822ade0e3a9dbb4fd2c90cdda3d0ff
mapilado128 <rolypoly128@gmail.com>
2024-04-09 19:30:23 -0700

    Menu button now working; copied script.js from main branch

--- static/script.js
+++ static/script.js
@@ -9,20 +9,6 @@ function reload(value){
         location.assign("/static/adminlogin.html");
     }
 }
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.sidebar').classList.toggle('active');
-    document.querySelector('.content').classList.toggle('active');
-});function reload(value){
-    if(value === "login"){
-        location.assign("/static/login.html");
-    }
-    else if(value === "register"){
-        location.assign("/static/registration.html");
-    }
-    else if(value === "adminlogin"){
-        location.assign("/static/adminlogin.html");
-    }
-}
 
 function passwordvalidate(){
     //Learned how to change form action from https://stackoverflow.com/a/5361776
