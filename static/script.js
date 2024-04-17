8eead847c0ba3be8a893c72851e64ecce3443ec5
mapilado128 <rolypoly128@gmail.com>
2024-04-17 12:50:57 -0700

    1. Added additional account balance boxes on member.html, 2. Changed the layout of transfer and deposit buttons to only appear when user hovers the specific account box, 3. Added JavaScript code to test out displaying account numbers into a span, 4. Added specific style on the js side of the account numbers to have it standout to make differentiating it easier, 5. Changed all paths in html files to relative paths

--- static/script.js
+++ static/script.js
@@ -88,6 +88,26 @@ function pin() {
     }
 }
 document.addEventListener('DOMContentLoaded', function() {
+    // Test account numbers
+    const accountNumbers = ['6789', '4321', '3579'];
+
+    // Gets all "des" div elements
+    const desDivs = document.querySelectorAll('.des');
+
+    // Loop through each "des" div and set the account number with span
+    desDivs.forEach((div, index) => {
+        // Create a span element for the account number
+        const span = document.createElement('span');
+        span.id = `accountNumber${index + 1}`; // Set the id for the span
+        span.textContent = accountNumbers[index]; // Set the account number text
+        span.style.color = '#f7c331';
+
+        // Append the span to "des" div
+        div.innerHTML = `Account Balance - #`;
+        div.appendChild(span);
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
     const sidebar = document.querySelector('.sidebar');
     const menuBtn = document.querySelector('.menu-btn');
 
