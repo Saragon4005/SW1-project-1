d874242b34492254ecd0505463bc277050eb14d4
Saragon4005 <raven.keszei@gmail.com>
2024-04-18 15:33:52 -0700

    format python

--- new_db.py
+++ new_db.py
@@ -1,5 +1,7 @@
 import os
 import sqlite3
+
+
 def script():
     if input("Do you want to delete current database? (N/y) ").lower() == "y":
         os.remove("bank.db")
@@ -7,7 +9,7 @@ def script():
     else:
         print("No changes made, exiting")
         return
-    
+
     print("Attempting to create new database")
 
     conn = sqlite3.Connection("bank.db", check_same_thread=False)
