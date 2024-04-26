2f655a8bf3dc563142771506ce599ce20848b044
Sai Mamidi <89061658+saimamidi01@users.noreply.github.com>
2024-04-25 17:40:36 -0700

    Initial Check deposit working

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ db = sqlite3.Connection("bank.db", check_same_thread=False)
 
 cursor = db.cursor()
 
-print(cursor.execute("SELECT account_number FROM accounts ORDER BY account_number DESC").fetchone())
+balance = cursor.execute("SELECT balance From accounts WHERE username='Sza'").fetchone()
 
+print(balance[0])
 
 
